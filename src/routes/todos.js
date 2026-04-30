64f36cdb6f86a49ef97c36732c83f21325aefe33
Segundo Oostdijk <seguoostdijk730@gmail.com>
2026-04-30 12:49:31 -0300

    feat(todos): add POST /todos endpoint

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -6,4 +6,13 @@ router.get('/', (req, res) => {
   res.json(store.getAll());
 });
 
+router.post('/', (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: 'title is required' });
+  }
+  const todo = store.create(title);
+  res.status(201).json(todo);
+});
+
 module.exports = router;
